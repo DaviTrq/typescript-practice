bb8e3a093da576c0db4e08be43959878f59ec59a
Davif <davitrqto@gmail.com>
2026-01-14 14:24:45 -0300

    feat: adding two more classes

--- dist/class07.js
+++ dist/class07.js
@@ -1,2 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+let things = [120, "test", true];
+things.push("hello", 360, true);
+things[2] = false;
+console.log(things);
+console.log(things[2]);
---
+++ dist/class08.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let data = {
+    name: "An",
+    age: 28,
+    status: "Active",
+    hello: () => { console.log("Hello"); },
+    info: (p) => { console.log(p); },
+};
+data.name = "Anna";
+console.log(typeof (data));
+console.log(data.name);
+data.hello();
+data.info("Welcome!");
